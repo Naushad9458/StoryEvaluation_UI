4e18163d4bacd8bd28267a8b3dcd2a46270b09c5
Naushad Alam <naushad@ProM1.local>
2023-07-26 14:12:39 +0100

    Initial Setup - Homepage, Navbar etc

---
+++ src/components/Home.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import {
+  Container,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button
+} from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  table: {
+    minWidth: 650,
+    marginTop: 20,
+  },
+}));
+
+const Home = () => {
+  // Sample data for pending and completed tasks
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      taskType: 'Pending',
+      completionDate: null,
+      description: 'Task 1 description',
+    },
+    {
+      id: 2,
+      taskType: 'Completed',
+      completionDate: '2023-07-24', // Replace with an actual completion date
+      description: 'Task 2 description',
+    },
+    // Add more tasks here...
+  ]);
+
+  const classes = useStyles();
+
+  return (
+    <div>
+    <Navbar/>
+    <Container>
+
+
+      <Typography variant="h6" component="h2" gutterBottom>
+        Pending Tasks
+      </Typography>
+      <TableContainer component={Paper} className={classes.table}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Task ID</TableCell>
+              <TableCell>Task Type</TableCell>
+              <TableCell>Description</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tasks
+              .filter((task) => task.taskType === 'Pending')
+              .map((task) => (
+                <TableRow key={task.id}>
+                  <TableCell>{task.id}</TableCell>
+                  <TableCell>{task.taskType}</TableCell>
+                  <TableCell>{task.description}</TableCell>
+                  <TableCell>
+                      <Button color="primary">
+                        View Details
+                      </Button>
+                    </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Typography variant="h6" component="h2" gutterBottom>
+        Completed Tasks
+      </Typography>
+      <TableContainer component={Paper} className={classes.table}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Task ID</TableCell>
+              <TableCell>Task Type</TableCell>
+              <TableCell>Completion Date</TableCell>
+              <TableCell>Description</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tasks
+              .filter((task) => task.taskType === 'Completed')
+              .map((task) => (
+                <TableRow key={task.id}>
+                  <TableCell>{task.id}</TableCell>
+                  <TableCell>{task.taskType}</TableCell>
+                  <TableCell>{task.completionDate}</TableCell>
+                  <TableCell>{task.description}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+    </div>
+  );
+};
+
+export default Home;
