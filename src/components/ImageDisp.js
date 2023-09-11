a345e3e1738b52ea9bffe4ff55bc19d329f9252b
Naushad Alam <ngc9458@gmail.com>
2023-09-11 15:16:24 +0100

    Testing without symbolic links

--- src/components/ImageDisp.js
+++ src/components/ImageDisp.js
@@ -53,7 +53,7 @@ export default function ImageDisplay(props) {
       {imageData.map((item) => (
         <ImageListItem key={item.image}>
           <img
-            //Add the image path here
+            //Add the image path here new
             src={`/images_files/${eventDate}/${eventDate}/${item.image}`}
             alt={`/images_files/${eventDate}/${eventDate}/${item.image}`}
             //src={`${process.env.PUBLIC_URL}/images_files/${eventDate}/${eventDate}/${item.image}`}
