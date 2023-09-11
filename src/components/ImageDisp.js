eff15234ac9506e234c6c5f39db23c731b9c0310
Naushad Alam <ngc9458@gmail.com>
2023-09-11 14:44:19 +0100

    Testing the images path

--- src/components/ImageDisp.js
+++ src/components/ImageDisp.js
@@ -54,8 +54,10 @@ export default function ImageDisplay(props) {
         <ImageListItem key={item.image}>
           <img
             //Add the image path here
-            src= {process.env.PUBLIC_URL + '/images_files/' + eventDate + '/' + eventDate + '/'+ item.image} 
-            alt={process.env.PUBLIC_URL + '/images_files/' + eventDate + '/' + eventDate + '/'+ item.image}
+            src={`${process.env.PUBLIC_URL}/images_files/${eventDate}/${eventDate}/${item.image}`}
+            alt={`${process.env.PUBLIC_URL}/images_files/${eventDate}/${eventDate}/${item.image}`}
+            //src= {process.env.PUBLIC_URL + '/images_files/' + eventDate + '/' + eventDate + '/'+ item.image} 
+            //alt={process.env.PUBLIC_URL + '/images_files/' + eventDate + '/' + eventDate + '/'+ item.image}
             //src= {process.env.PUBLIC_URL + '/' + eventDate + '/' + eventDate + '/'+ item.image} 
             //alt={process.env.PUBLIC_URL + '/' + eventDate + '/' + eventDate + '/'+ item.image}
             loading="lazy"
