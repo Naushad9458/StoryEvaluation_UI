e5ffa700c579e255f07306ba432c70e930e6c5b4
Naushad Alam <ngc9458@gmail.com>
2023-10-02 13:20:13 +0100

    Adding a back button on navbar, removing the menu icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,14 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 
 
 const Navbar = (props) => {
+
+  
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -21,7 +25,7 @@ const Navbar = (props) => {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <ArrowBackIcon/>
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Lifelog Narrative Evaluation 
