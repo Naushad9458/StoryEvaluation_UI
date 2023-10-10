91efb0d9cb5fd17595ed43cb4c5bbff62dc102a0
Naushad Alam <ngc9458@gmail.com>
2023-10-10 12:36:45 +0100

    Fixing typo in Intructions Alert Box

--- src/components/AlertInstructions.js
+++ src/components/AlertInstructions.js
@@ -27,9 +27,9 @@ export default function AlertInstructions(props) {
             <p>2. Then <b>form a narrative in your mind</b> based the images.<br/></p>
             <p>3. Go through the narrative <b>answer the 3 questions first.</b><br/></p>
             <p>4. We also ask you to provide a correct narrative if you think the narrative is wrong by making minimal changes and keeping the fluency and coherency of the narrative intact. <br/></p>
-            <p style={{color:'red'}}>6. Always answer the questions first based on the original narratives and then proceed to editing if need be and finally click 'Submit'</p>
-            <p><b>5. There are multiple narratives for each set of images. Repeat steps 1-4 for each of narratives displayed</b> <br/></p>
-            <p>6. The scores you assign to narratives (for the 3 questions) are relative. A strong analogy could be when you are marking student's answer sheets in a class <br/></p>
+            <p style={{color:'red'}}>5. Always answer the questions first based on the original narratives and then proceed to editing if need be and finally click 'Submit'</p>
+            <p><b>6. There are multiple narratives for each set of images. Repeat steps 1-4 for each of narratives displayed</b> <br/></p>
+            <p>7. The scores you assign to narratives (for the 3 questions) are relative. A strong analogy could be when you are marking student's answer sheets in a class <br/></p>
           </DialogContentText>
         </DialogContent>
         <DialogActions>
