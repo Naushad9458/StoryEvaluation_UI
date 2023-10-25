fb261ece2b4d820fcca049a1e2e4c75a168e797c
Naushad Alam <ngc9458@gmail.com>
2023-10-25 14:46:15 +0100

    Removing arrow back button from toolbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,6 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 
 const Navbar = (props) => {
@@ -15,15 +13,7 @@ const Navbar = (props) => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <ArrowBackIcon/>
-          </IconButton>
+          
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Lifelog Narrative Evaluation 
           </Typography>
