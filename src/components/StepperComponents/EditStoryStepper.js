bc8debbb9aa11af62db17229c16ffe5c5e4029f4
Naushad Alam <ngc9458@gmail.com>
2023-11-17 15:43:30 +0000

    Add code to calc edit percentage, modify code for cancelEdit and confirmEdit

--- src/components/StepperComponents/EditStoryStepper.js
+++ src/components/StepperComponents/EditStoryStepper.js
@@ -17,27 +17,24 @@ const EditStoryStepper = (props) => {
     const {questions} = props;
     const {sliderValues} = props;
     const {story} = props;
-    const {editedStories} = props;
-    const {editedStoryKey} = props;
-    const {setEditedStories} = props;
+    const {editedStory} = props;
+    const {setEditedStory} = props;
     const {setEditDistance} = props;
     const {editDistance} = props;
+    const {setEditPercentage} = props;
 
     const [alertOpen, setAlertOpen] = React.useState(false);
     const [editDisabled, setEditDisabled] = React.useState(true);
-
     const [confirmEditDisabled, setConfirmEditDisabled] = React.useState(true);
     const [cancelEditDisabled, setCancelEditDisabled] = React.useState(true);
     
-
-    const editedStoriesCopy = { ...editedStories };
-
+    const editedStoryCopy = { ...editedStory };
 
     const story_modify = (event) => {
       setConfirmEditDisabled(false);
       setCancelEditDisabled(false);
-      editedStoriesCopy[editedStoryKey] = event.target.value;
-      setEditedStories(editedStoriesCopy);
+      editedStoryCopy['narrative_text'] = event.target.value;
+      setEditedStory(editedStoryCopy);
     }
 
 
@@ -45,80 +42,96 @@ const EditStoryStepper = (props) => {
         setEditDisabled(false);
     };
 
+    const handleTextFieldClick = () => {
+      console.log('handleTextFieldClick')
+      setEditDisabled(false);
+    };
+
     const cancelStoryEdit = () => {
       setAlertOpen(false);
-      editedStoriesCopy[editedStoryKey] = story
-      setEditedStories(editedStoriesCopy)
+      setEditDistance(0);
+      setEditPercentage(0);
+      setEditedStory(story)
       setEditDisabled(true);
       setConfirmEditDisabled(true);
       setCancelEditDisabled(true);
+    };
+
+    const cancelAllEdit = () => {
+      setEditedStory(story)
+      setEditDisabled(true);
+      setConfirmEditDisabled(true);
+      setCancelEditDisabled(true);
+      setEditDistance(0);
+      setEditPercentage(0);
     }
 
     const alerthandleClickOpen = () => {
-        var lev = ed.levenshtein(story, editedStories[editedStoryKey] , insert, remove, update);
-        let EditDistanceCopy = [ ...editDistance ];
-        EditDistanceCopy[editedStoryKey] = lev.distance
-        setEditDistance(EditDistanceCopy)
-        setAlertOpen(true);
-      };
+        var lev = ed.levenshtein(story['narrative_text'], editedStory['narrative_text'] , insert, remove, update);
+        setEditDistance(lev.distance)
+        var len_story = story['narrative_text'].length
+        var len_editedStory = editedStory['narrative_text'].length
+        var max_len = Math.max(len_story, len_editedStory)
+        var normalized_edit_distance = (max_len - lev.distance) / max_len;
+        var edit_percentage = ((1- normalized_edit_distance) * 100).toFixed(1);;
 
-      const alerthandleClose = () => {
+        console.log(normalized_edit_distance, 'normalized_edit_distance')
+        console.log(edit_percentage, 'edit_percentage')
+        setEditPercentage(edit_percentage)
+        setAlertOpen(true);
+    };
+
+    const alerthandleClose = () => {
         setAlertOpen(false);
-        setEditedStories(editedStoriesCopy)
+        setEditedStory(editedStoryCopy)
         setEditDisabled(true);
         setConfirmEditDisabled(true);
         setCancelEditDisabled(true);
         //console.log(editedStories[displayOrder[activeStep]], 'confirm edit story click')
         //console.log(editDisabled, 'confirm edit story click')
   
-      };
+    };
 
     return (
     <div>
 
         {questions.map((tile) => (
-        <div key={tile.question_id}>
+        <div key={tile.ques_id}>
         <Box>
           <p key={tile.ques_id} style={{fontSize: '18px'}}>
             {tile.ques_text}
           </p>
           <div >
-            
-            <Slider
-              defaultValue={sliderValues[tile.ques_id]}
-              aria-label="Default"
-              disabled={true}
-              valueLabelDisplay="on"
-            />
+          <p>Score: <b>{sliderValues[tile.ques_id]}</b></p>
           </div>
         </Box>
       </div>
         ))}
 
-    
     <p><b>Original Narrative</b></p>
-
     <TextField 
     fullWidth
     disabled={true}
     multiline
     rows={8}
-    value={story}
-    InputProps={{ style: { fontSize: 18 } }}>
+    value={story['narrative_text']}
+    InputProps={{ style: { fontSize: 18} }}>
     </TextField>
-    <AlertDialog open={alertOpen} handleConfirm={alerthandleClose} handleCancel={cancelStoryEdit} handleClickOpen={alerthandleClickOpen} editDistance={editDistance[editedStoryKey]}/>
+    <AlertDialog open={alertOpen} handleConfirm={alerthandleClose} handleCancel={cancelStoryEdit} handleClickOpen={alerthandleClickOpen} editDistance={editDistance}/>
     <p><b>Edited Narrative</b></p>
     <Button onClick={editStoryHandle}>Edit Narrative</Button>
-    <Button disabled={confirmEditDisabled} onClick={alerthandleClickOpen}>Confirm Edit</Button>
-    <Button disabled={cancelEditDisabled} onClick={cancelStoryEdit}>Cancel Edit</Button>
+    <Button disabled={confirmEditDisabled} onClick={alerthandleClickOpen}>Confirm All Edits</Button>
+    <Button onClick={cancelAllEdit}>Cancel All Edits</Button>
     <TextField 
     fullWidth
     disabled={editDisabled}
     multiline
     rows={8}
-    value={editedStories[editedStoryKey]}
+    value={editedStory['narrative_text']}
     onChange={story_modify}
-    InputProps={{style:{fontSize: 18}}}>
+    InputProps={{style:{fontSize: 18}
+  }
+    }>
     </TextField>
     </div>          
     );
